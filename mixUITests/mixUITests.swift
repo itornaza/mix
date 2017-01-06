9e989e1a4223fd0c7f948dd6439f8ab39089dde0
Ioannis Tornazakis <itornaza@gmail.com>
2017-01-06 21:41:20 +0200

    added some ui tests

--- mixUITests/mixUITests.swift
+++ mixUITests/mixUITests.swift
@@ -9,7 +9,7 @@
 import XCTest
 
 class mixUITests: XCTestCase {
-        
+    
     override func setUp() {
         super.setUp()
         
@@ -28,9 +28,48 @@ class mixUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testUIElements() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let app = XCUIApplication()
+        
+        // Check elements exist
+        XCTAssertEqual(app.textFields.count, 2)
+        XCTAssertNotNil(app.buttons["1%"])
+        XCTAssertNotNil(app.buttons["2%"])
+        XCTAssertNotNil(app.textFields["waterVolume"])
+        XCTAssertNotNil(app.textFields["mixtureConcentration"])
+        XCTAssertEqual(app.staticTexts.matching(identifier: "result").count, 1)
+        
+        // Check results title is set appropriately
+        app.buttons["Reset"].tap()
+        app.buttons["1%"].tap()
+        XCTAssertNotNil(app.staticTexts["Lidocaine 1%"])
+        app.buttons["2%"].tap()
+        XCTAssertNotNil(app.staticTexts["Lidocaine 2%"])
+
+        app.textFields["waterVolume"].tap()
+        app.textFields["waterVolume"].clearAndEnterText(text: "250")
+        app.textFields["mixtureConcentration"].tap()
+        app.textFields["mixtureConcentration"].clearAndEnterText(text: "0.5")
     }
     
+}
+
+extension XCUIElement {
+    /**
+     Removes any current text in the field before typing in the new value
+     - Parameter text: the text to enter into the field
+     */
+    func clearAndEnterText(text: String) -> Void {
+        guard let stringValue = self.value as? String else {
+            XCTFail("Tried to clear and enter text into a non string value")
+            return
+        }
+        
+        self.tap()
+        let deleteString = stringValue.characters.map { _ in XCUIKeyboardKeyDelete }.joined(separator: "")
+        self.typeText(deleteString)
+        self.typeText(text)
+    }
 }
